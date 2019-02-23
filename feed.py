ffc21b992aaf4f1ee8c6ff9612a2525ed3a4f364
Don Walling <dwalling@cyas.com>
2019-02-23 15:00:07 -0800

    Add 100ms delay to turn off motor so tray lines up better

--- feed.py
+++ feed.py
@@ -6,10 +6,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(25, GPIO.IN)
 
-time.sleep(0.100)
-
 while True:
 	if GPIO.input(25):
+		time.sleep(0.100)
 		GPIO.output(18,False)
 		break
 	else:
